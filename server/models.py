435293e52a22be969c6ff4c7053bb729c29f2b2a
Steve <git@github.com:SKaranjaN/Database.git>
2023-06-27 18:03:27 +0300

    Imports, Dependencies, Tables

---
+++ server/models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+
+    comments = db.relationship("Comment", backref = "user")
+
+    def __repr__(self):
+        return f'<User: {self.name}>'
+
+
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    post = db.Column(db.String())
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'<Comment: {self.post}>'
